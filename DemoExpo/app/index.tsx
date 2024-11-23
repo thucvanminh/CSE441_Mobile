e27a0565c1549cb03fb34c78ea5decdf3aac240e
thucvanminh <thucnichan2929vmt@gmail.com>
2024-11-23 11:10:09 +0700

    Quick

---
+++ DemoExpo/app/index.tsx
@@ -0,0 +1,16 @@
+import { Image, StyleSheet, Platform, Text, View } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+
