b4fa7c8d12dc13515c2c7e2ddc8d62835d571be7
thucvanminh <thucnichan2929vmt@gmail.com>
2024-11-30 12:53:44 +0700

    Quick

--- DemoExpoo/app/index.tsx
+++ DemoExpoo/app/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -8,9 +8,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 
 
-// import * as React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
+
+
 
 const Stack = createStackNavigator();
 
